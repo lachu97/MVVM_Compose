57bc965eb504f627d6e4de0f723a1f5dd7bc550b
lakshu <ced15i004@iiitdm.ac.in>
2022-04-06 16:18:45 +0530

    Generics Select and Fetch items 56

--- app/src/main/java/com/example/composeactive/navigation/nav.kt
+++ app/src/main/java/com/example/composeactive/navigation/nav.kt
@@ -4,4 +4,6 @@ sealed class Screen(val route:String){
     object Home:Screen(route = "home")
     object detail:Screen(route = "detail")
     object moredetail:Screen(route = "moredetail")
+    object SomeMoreDeail:Screen(route = "SomeDetail")
+    object Extradetail:Screen(route = "Extradetail")
 }
